a1d59376888cdf7ff7f0989608a0c4c67eac679d
Ahkeem Lang <langahkeem@gmail.com>
2016-06-17 13:42:50 -0500

    I reassigned the the player_movement arguments and in turn I changed the grid.gird[] = to character

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -7,82 +7,83 @@ class Player
   def initialize(name)
     @name = name
   end
-  def player_movement(grid)
+  def player_movement(grid, character)
     if player_one.input == '1'
       puts "You chose cell 1"
-      grid.grid['1'] = "x"
+      grid.grid['1'] = character
+      puts ""
     end
     if player_one.input == '2'
       puts "You chose cell 2"
-      grid.grid['2'] = "x"
+      grid.grid['2'] = character
     end
     if player_one.input == '3'
       puts "You chose cell 3"
-      grid.grid['3'] = "x"
+      grid.grid['3'] = character
     end
     if player_one.input == '4'
       puts "You chose cell 4"
-      grid.grid['4'] = "x"
+      grid.grid['4'] = character
     end
     if player_one.input == '5'
       puts "You chose cell 5"
-      grid.grid['5'] = "x"
+      grid.grid['5'] = character
     end
     if player_one.input == '6'
       puts "You chose cell 6"
-      grid.grid['6'] = "x"
+      grid.grid['6'] = character
     end
     if player_one.input == '7'
       puts "You chose cell 7"
-      grid.grid['7'] = "x"
+      grid.grid['7'] = character
     end
     if player_one.input == '8'
       puts "You chose cell 8"
-      grid.grid['8'] = "x"
+      grid.grid['8'] = character
     end
     if player_one.input == '9'
       puts "You chose cell 9"
-      grid.grid['9'] = "9"
+      grid.grid['9'] = character
     end
 
   end
 
-  def computer_movement(grid)
+  def computer_movement(grid, character)
     if computer_ai.input == '1'
       puts "You chose cell 1"
-      grid.grid['1'] = "x"
+      grid.grid['1'] = character
     end
     if computer_ai.input == '2'
       puts "You chose cell 2"
-      grid.grid['2'] = "x"
+      grid.grid['2'] = character
     end
     if computer_ai.input == '3'
       puts "You chose cell 3"
-      grid.grid['3'] = "x"
+      grid.grid['3'] = character
     end
     if computer_ai.input == '4'
       puts "You chose cell 4"
-      grid.grid['4'] = "x"
+      grid.grid['4'] = character
     end
     if  computer_ai.input == '5'
       puts "You chose cell 5"
-      grid.grid['5'] = "x"
+      grid.grid['5'] = character
     end
     if computer_ai.input == '6'
       puts "You chose cell 6"
-      grid.grid['6'] = "x"
+      grid.grid['6'] = character
     end
     if computer_ai.input == '7'
       puts "You chose cell 7"
-      grid.grid['7'] = "x"
+      grid.grid['7'] = character
     end
     if computer_ai.input == '8'
       puts "You chose cell 8"
-      grid.grid['8'] = "x"
+      grid.grid['8'] = character
     end
     if computer_ai.input == '9'
       puts "You chose cell 9"
-      grid.grid['9'] = "9"
+      grid.grid['9'] = character
     end
   end
 
@@ -91,6 +92,12 @@ class Player
     print "Choose a section. "
     choices.each do |choice|
       print "#{choice} "
+      def players_symbols
+        player_one.player_movement(grid) == "X"
+      end
+      def computer_symbols
+        computer_ai.computer_movement(grid) == "O"
+      end
     end
     @section = gets.chomp
   end
@@ -153,5 +160,5 @@ player_one = Player.new(name)
 player_one.input
 computer_ai.input
 
-player_one.player_movement(grid)
+player_one.player_movement(grid, )
 computer_ai.computer_movement(grid)
