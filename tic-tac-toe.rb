e8adbeb4342329deea1571752ed2c61f273d7e5b
Ahkeem Lang <langahkeem@gmail.com>
2016-08-23 15:00:34 -0500

    Fixing a undefined method issue

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -246,166 +246,144 @@ def reset_board
     board_moves
   end
 end
+
+def x_marks
+  if @player_one.include?(1) then
+    @grid1 = "X"
+  end
+  if @player_one.include?(2) then
+    @grid2 = "X"
+  end
+  if @player_one.include?(3) then
+    @grid3 = "X"
+  end
+  if @player_one.include?(4) then
+    @grid4 = "X"
+  end
+  if @player_one.include?(5) then
+    @grid5 = "X"
+  end
+  if @player_one.include?(6) then
+    @grid6 = "X"
+  end
+  if @player_one.include?(7) then
+    @grid7 = "X"
+  end
+  if @player_one.include?(8) then
+    @grid8 = "X"
+  end
+  if @player_one.include?(9) then
+    @grid9 = "X"
+  end
+end
+
+def o_marks
+  if @player_two.include?(1) then
+    @grid1 = "O"
+  end
+  if @player_two.include?(2) then
+    @grid2 = "O"
+  end
+  if @player_two.include?(3) then
+    @grid3 = "O"
+  end
+  if @player_two.include?(4) then
+    @grid4 = "O"
+  end
+  if @player_two.include?(5) then
+    @grid5 = "O"
+  end
+  if @player_two.include?(6) then
+    @grid6 = "O"
+  end
+  if @player_two.include?(7) then
+    @grid7 = "O"
+  end
+  if @player_two.include?(8) then
+    @grid8 = "O"
+  end
+  if @player_two.include?(9) then
+    @grid9 = "O"
+  end
+end
+
+def board_moves
+  system("clear")
+  x_marks
+  o_marks
+  puts "    Tic-tac-toe    "
+  puts "    #{@header}     "
+  puts "╔════════╦════════╗"
+  puts "║ X = #{@player_one_score}  ║ O = #{@player_two_score}  ║"
+  puts "╚════════╩════════╝"
+  puts "   ┍━━━━━━━━━━━┑"
+  puts "   ⎟ #{@grid1} ║ #{@grid2} ║ #{@grid3} ⎟"
+  puts "   ⎟═══╬═══╬═══⎟"
+  puts "   ⎟ #{@grid4} ║ #{@grid5} ║ #{@grid6} ⎟"
+  puts "   ⎟═══╬═══╬═══⎟"
+  puts "   ⎟ #{@grid7} ║ #{@grid8} ║ #{@grid9} ⎟"
+  puts "   ┕━━━━━━━━━━━┙"
+end
+
+def boot_game
+  def to_start
+    puts "Well, shall we do this?"
+    print "(y)es or (n)o?> "
+    start = gets.chomp
+    if start.upcase == "Y"
+      puts "LETS GET THIS PARTY STARTED"
+      3.times do
+        sleep 0.5
+        # board_moves
+        print "!"
+        # board_moves
+      end
+    elsif start.upcase == "N"
+      puts "Right"
+      3.times do
+        sleep 0.5
+        print "."
+      end
+      print "so"
+      sleep 3
+      to_start
+    else
+      puts "YO! try that again!"
+      sleep 1
+      to_start
+    end
+  end
+end
+
+def to_opponent
+    puts "PvP or PvC"
+    print "(c)omputer or (p)layer?> "
+    opponent = gets.chomp
+    if opponent.upcase == "C"
+      @twoplayer = FALSE
+      puts "ITS COMPUTER!"
+      sleep 3
+    elsif opponent.upcase == "P"
+      @twoplayer = true
+      puts "ITS ANOTHER PLAYER!"
+      sleep 3
+    else
+      puts "YO! try that again!"
+      sleep 1
+      to_opponent
+    end
+end
+  to_start
+  to_opponent
+  @header = " BATTLE! "
+
+
+def go
+  reset_board
+  play_game
+end
+
+
+boot_game
+go
 action
-
-
-
-#
-#   def movement(grid, character, spot)
-#     if spot == '1'
-#       puts "You chose cell 1"
-#       grid.grid['1'] = character
-#     end
-#     if spot == '2'
-#       puts "You chose cell 2"
-#       grid.grid['2'] = character
-#     end
-#     if spot == '3'
-#       puts "You chose cell 3"
-#       grid.grid['3'] = character
-#     end
-#     if spot == '4'
-#       puts "You chose cell 4"
-#       grid.grid['4'] = character
-#     end
-#     if spot == '5'
-#       puts "You chose cell 5"
-#       grid.grid['5'] = character
-#     end
-#     if spot == '6'
-#       puts "You chose cell 6"
-#       grid.grid['6'] = character
-#     end
-#     if spot == '7'
-#       puts "You chose cell 7"
-#       grid.grid['7'] = character
-#     end
-#     if spot == '8'
-#       puts "You chose cell 8"
-#       grid.grid['8'] = character
-#     end
-#     if spot == '9'
-#       puts "You chose cell 9"
-#       grid.grid['9'] = character
-#     end
-#
-#   end
-#
-#
-# # end
-#
-#
-#
-#
-#
-#
-# class Grid
-#   attr_accessor :grid
-#   def initialize
-#     @symbols = []
-#     @grid = {
-#        '1' => '1',
-#        '2' => '2',
-#        '3' => '3',
-#        '4' => '4',
-#        '5' => '5',
-#        '6' => '6',
-#        '7' => '7',
-#       '8' => '8',
-#       '9' => '9',
-#     }
-#   end
-#
-#
-#
-#   def print_grid
-#     puts """
-#     #{@grid['1']} | #{@grid['2']} | #{@grid['3']}
-#     ------------
-#     #{@grid['4']}| #{@grid['5']}  | #{@grid['6']}
-#     ------------
-#     #{@grid ['7']}| #{@grid['8']}  | #{@grid ['9']}
-#     """
-#   end
-# end
-#
-#
-# #methods
-# def choice
-#   movement.each do |character, spot|
-#     if spot != @grid
-#       not_@grid.push(character)
-#     end
-#   end
-# end
-#
-# def record
-#   if player_section.movement == """ #{@grid ['1']} && #{@grid ['2']} && #{@grid ['3']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-#   if player_section.movement == """ #{@grid ['1']} && #{@grid ['5']} && #{@grid ['9']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-#   if player_section.movement == """ #{@grid ['1']} && #{@grid ['4']} && #{@grid ['7']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-#   if player_section.movement == """ #{@grid ['4']} && #{@grid ['5']} && #{@grid ['6']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-#   if player_section.movement ==""" #{@grid ['7']} && #{@grid ['8']} && #{@grid ['9']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-#   if player_section.movement == """ #{@grid ['7']} && #{@grid ['5']} && #{@grid ['3']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-#   if player_section.movement == """ #{@grid ['3']} && #{@grid['6']} && #{@grid['9']}"""
-#     puts "You win!"
-#   else
-#     puts "Better luck next time."
-#   end
-# end
-#
-# #methods
-#
-#
-# puts "Welcome to Tic-Tac-Toe, play for complete domination!"
-#
-# grid = Grid.new
-#
-# print "<Player one type your name here:>"
-# name = gets.chomp
-#
-# player_one = Player.new(name)
-# computer_ai = Player.new("Computer")
-#
-# loop do
-#   #Start of the turn
-#   print "To play you have to choose through sections 1-9,"
-#
-#   spot = gets.chomp
-#
-#   grid.print_grid
-#
-#   player_section   = "X"
-#   computer_section = "O"
-#
-#   player_one.movement(grid, player_section, spot)
-#   computer_ai.movement(grid, computer_section, rand(1..9).to_s)
-#   # Computer choice cannot stay random forever, will need to pick from available moes
-#
-#
-#   grid.print_grid
-#   #End of the turn
-#end
